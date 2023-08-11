5821b288468774a36716041177b5916eebf7d2dc
jitae98 <jitae98tu@naver.com>
2023-08-11 20:15:50 +0700

    loginandregister

--- Register/script.js
+++ Register/script.js
@@ -11,37 +11,37 @@ form.addEventListener("submit", (e) => {
 
 function checkInputs() {
   const usernameValue = username.value.trim();
-  const email = email.value.trim();
+  const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
-  const password2Value = password.value.trim();
-}
+  const password2Value = password2.value.trim();
 
-if (username === "") {
-  setErrorFor(username, "Username cannot be blank");
-} else {
-  setSuccessFor(username);
-}
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  } else {
+    setSuccessFor(username);
+  }
 
-if (emailValue === "") {
-  setErrorFor(email, "Email cannot be blank");
-} else if (!isEmail(emailValue)) {
-  setErrorFor(email, "Not a valid email");
-} else {
-  setSuccessFor(email);
-}
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Not a valid email");
+  } else {
+    setSuccessFor(email);
+  }
 
-if (passwordValue === "") {
-  setErrorFor(password, "Password cannot be blank");
-} else {
-  setSuccessFor(password);
-}
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else {
+    setSuccessFor(password);
+  }
 
-if (password2Value === "") {
-  setErrorFor(password2, "Password2 cannot be blank");
-} else if (passwordValue !== password2Value) {
-  setErrorFor(password2, "Passwords does not match");
-} else {
-  setSuccessFor(password2);
+  if (password2Value === "") {
+    setErrorFor(password2, "Password2 cannot be blank");
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, "Passwords do not match");
+  } else {
+    setSuccessFor(password2);
+  }
 }
 
 function setErrorFor(input, message) {
@@ -57,7 +57,7 @@ function setSuccessFor(input) {
 }
 
 function isEmail(email) {
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(",+"))@((\[[0-9]{1,3}\.[0,9]{1,3}\.[0,9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(",+"))@((\[[0-9]{1,3}\.[0,9]{1,3}\.[0,9]{1,3}])|(([a-zA-Z\-0,9]+\.)+[a-zA-Z]{2,}))$/.test(
     email
   );
 }
