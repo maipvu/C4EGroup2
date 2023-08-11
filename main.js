8855234d7098f7cc831931bae357b5d23e474e5e
dophambichtram <bichtramdopham@gmail.com>
2023-08-11 23:39:41 +0700

    update

--- main.js
+++ main.js
@@ -11,17 +11,3 @@ btnClose.addEventListener("click", function () {
   menu.classList.remove("menu--active");
 });
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  hashNavigation: {
-      watchState: true,
-  },
-  pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-  },
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-});
