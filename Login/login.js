805e55019bc9636b074ae8485694193a714a5b60
jitae98 <jitae98tu@naver.com>
2023-08-20 18:16:18 +0700

    updateLoginpage

---
+++ Login/login.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginButton = document.getElementById("loginButton");
+  const loginUsername = document.getElementById("loginUsername");
+  const loginPassword = document.getElementById("loginPassword");
+
+  loginButton.addEventListener("click", (e) => {
+    checkLogin();
+  });
+
+  loginPassword.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkLogin();
+    }
+  });
+
+  function checkLogin() {
+    const enteredUsername = loginUsername.value.trim();
+    const enteredPassword = loginPassword.value.trim();
+
+    const storedUsername = localStorage.getItem("username");
+    const storedPassword = localStorage.getItem("password");
+
+    if (
+      enteredUsername === storedUsername &&
+      enteredPassword === storedPassword
+    ) {
+      alert("Login successful. Welcome back," + enteredUsername + "!");
+      window.location.href = "../../C4EGroup2/index.html";
+    } else {
+      alert(
+        "Username and password are incorrect. Please login again or Register new account!"
+      );
+    }
+  }
+});
+
+const registerButton = document.createElement("button");
+registerButton.innerText = "REGISTER";
+registerButton.addEventListener("click", () => {
+  window.location.href = "../Register/register.html";
+});
+document.querySelector(".login").appendChild(registerButton);
